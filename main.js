108cd7dc2bcac267f869f668fb8fd4a2e6d15190
JAOCruz <notmeee00@gmail.com>
2025-08-22 11:55:15 -0400

    Draft

---
+++ main.js
@@ -0,0 +1,31 @@
+// Import CSS files
+import './css/bootstrap.css'
+import './css/font-awesome.min.css'
+import './css/linearicons.css'
+import './css/magnific-popup.css'
+import './css/nice-select.css'
+import './css/animate.min.css'
+import './css/owl.carousel.css'
+import './css/main.css'
+import './css/custom-styles.css'
+
+// Import JavaScript files
+import './js/vendor/jquery-2.2.4.min.js'
+import './js/easing.min.js'
+import './js/hoverIntent.js'
+import './js/superfish.min.js'
+import './js/jquery.ajaxchimp.min.js'
+import './js/jquery.magnific-popup.min.js'
+import './js/owl.carousel.min.js'
+import './js/imagesloaded.pkgd.min.js'
+import './js/justified.min.js'
+import './js/jquery.sticky.js'
+import './js/jquery.nice-select.min.js'
+import './js/parallax.min.js'
+import './js/mail-script.js'
+import './js/main.js'
+
+// Initialize when DOM is ready
+document.addEventListener('DOMContentLoaded', function() {
+  console.log('Segunda Casa - Arte Actual website loaded successfully!')
+}) 
---
+++ vite.config.js
@@ -0,0 +1,43 @@
+import { defineConfig } from 'vite'
+import { resolve } from 'path'
+
+export default defineConfig({
+  root: '.',
+  publicDir: 'public',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: false,
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+        about: resolve(__dirname, 'about.html'),
+        gallery: resolve(__dirname, 'gallery.html'),
+        event: resolve(__dirname, 'event.html'),
+        ticket: resolve(__dirname, 'ticket.html'),
+        blog: resolve(__dirname, 'blog-home.html'),
+        'blog-single': resolve(__dirname, 'blog-single.html'),
+        category: resolve(__dirname, 'category.html'),
+        elements: resolve(__dirname, 'elements.html'),
+        contact: resolve(__dirname, 'contact.html')
+      }
+    }
+  },
+  server: {
+    port: 3000,
+    open: true,
+    host: true
+  },
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '~': resolve(__dirname, './')
+    }
+  },
+  optimizeDeps: {
+    exclude: ['js/vendor/bootstrap.min.js']
+  },
+  esbuild: {
+    logOverride: { 'this-is-undefined-in-esm': 'silent' }
+  }
+}) 
